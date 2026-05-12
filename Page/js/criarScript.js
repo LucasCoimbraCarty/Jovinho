4e786ec4fa0ec8d8d0314985ef0c82ff9458f92f
correaleite <gabriel@correaleite.com.br>
2026-05-12 11:25:28 -0300

    alert adicionado

--- Page/js/criarScript.js
+++ Page/js/criarScript.js
@@ -10,18 +10,23 @@ const senha = document.getElementById('IDsenha').value.trim();
 
 
    if( email.indexOf("@") !== -1 && senha.length >= 5){
-    display.innerText = 'Conta criada com sucesso!'
+   //  display.innerText = 'Conta criada com sucesso!'
+   alert('Conta criada com sucesso!')
+
    }
    else if(email.indexOf("@") == -1 && senha.length >= 5) {
-    display.innerText = 'Email inválido';
+   //  display.innerText = 'Email inválido';
+    alert('Email inválido')
     console.log(`email inválido: ${email}`);
    } 
    else if(email.indexOf("@") !== -1 && senha.length <= 5) {
-    display.innerText = 'A senha precisa de no mínimo 5 caracteres';
+   //  display.innerText = 'A senha precisa de no mínimo 5 caracteres';
+    alert('A senha precisa de no mínimo 5 caracteres')
     console.log(`senha inválida: ${senha}`);
    }
    else {
-    display.innerText = 'Email e senha inválidos';
+   //  display.innerText = 'Email e senha inválidos';
+    alert('Email e senha inválidos')
     console.log(`email: ${email} e senha : ${senha}`);
    }
 }
--- Page/js/loginScript.js
+++ Page/js/loginScript.js
@@ -9,10 +9,12 @@ const email = document.getElementById('IDemail').value.trim();
 const senha = document.getElementById('IDsenha').value.trim();
 
    if( email == "jovinho@fiap.com.br" && senha == "123"){
-    display.innerText = 'Conta criada com sucesso!'
+   //  display.innerText = 'Conta logada com sucesso!'
+   alert('Conta logada com sucesso!')
    }
    else {
-    display.innerText = 'Email ou senha incorretos';
+   //  display.innerText = 'Email ou senha incorretos';
+    alert('Conta logada com sucesso!')
     console.log(`email: ${email} e senha : ${senha}`);
    }
 }
