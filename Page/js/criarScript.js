1429148994a928b234db98e11bdfe6f273fe37f4
correaleite <gabriel@correaleite.com.br>
2026-05-12 17:00:30 -0300

    js finalizado

--- Page/js/criarScript.js
+++ Page/js/criarScript.js
@@ -1,31 +1,21 @@
-
-const display = document.getElementById("display");
 const botao = document.getElementById("btnLogar");
 
-
 function criarConta(){
-
 const email = document.getElementById('IDemail').value.trim();
 const senha = document.getElementById('IDsenha').value.trim();
 
-
    if( email.indexOf("@") !== -1 && senha.length >= 5){
-   //  display.innerText = 'Conta criada com sucesso!'
    alert('Conta criada com sucesso!')
-
    }
    else if(email.indexOf("@") == -1 && senha.length >= 5) {
-   //  display.innerText = 'Email inválido';
     alert('Email inválido')
     console.log(`email inválido: ${email}`);
    } 
    else if(email.indexOf("@") !== -1 && senha.length <= 5) {
-   //  display.innerText = 'A senha precisa de no mínimo 5 caracteres';
     alert('A senha precisa de no mínimo 5 caracteres')
     console.log(`senha inválida: ${senha}`);
    }
    else {
-   //  display.innerText = 'Email e senha inválidos';
     alert('Email e senha inválidos')
     console.log(`email: ${email} e senha : ${senha}`);
    }
--- Page/js/loginScript.js
+++ Page/js/loginScript.js
@@ -1,22 +1,34 @@
-
-const display = document.getElementById("display");
 const botao = document.getElementById("btnLogar");
-
+const botaoReport = document.getElementById("btnReportar");
 
 function logar(){
-
 const email = document.getElementById('IDemail').value.trim();
 const senha = document.getElementById('IDsenha').value.trim();
 
    if( email == "jovinho@fiap.com.br" && senha == "123"){
-   //  display.innerText = 'Conta logada com sucesso!'
    alert('Conta logada com sucesso!')
    }
+   else if(email.indexOf("@") == -1 && senha.length >= 5) {
+    alert('Email inválido')
+    console.log(`email inválido: ${email}`);
+   } 
+   else if(email.indexOf("@") !== -1 && senha.length <= 5) {
+    alert('A senha precisa de no mínimo 5 caracteres')
+    console.log(`senha inválida: ${senha}`);
+   }
    else {
-   //  display.innerText = 'Email ou senha incorretos';
-    alert('Conta logada com sucesso!')
+    alert('Email ou senha incorretos')
     console.log(`email: ${email} e senha : ${senha}`);
    }
 }
 
+function Reportar() {
+  let report = prompt("Reporte o problema em seu login", "");
+  if (report != null) {
+    alert(`verificaremos a sua denuncia: "${report}"`) 
+  }
+  console.log(`Report: ${report}`)
+}
+
 botao.addEventListener("click", logar); 
+botaoReport.addEventListener("click", Reportar); 
